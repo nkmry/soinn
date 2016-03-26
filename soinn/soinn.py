6acae043f7e6c4611dbd55bbcfa4799cb3c29ccc
fukatani <nannyakannya@gmail.com>
2016-03-26 17:07:45 +0900

    for speeding up __delete_noise_nodes

--- soinn/soinn.py
+++ soinn/soinn.py
@@ -98,7 +98,7 @@ class Soinn(object):
                 pal_indexes = []
                 for k in pals.keys():
                     pal_indexes.append(k[1])
-                sq_dists = np.sum((self.nodes[pal_indexes] - np.stack([self.nodes[i] for j in range(len(pal_indexes))]))**2, 1)
+                sq_dists = np.sum((self.nodes[pal_indexes] - np.array([self.nodes[i] * len(pal_indexes)]))**2, 1)
                 sim_thresholds.append(np.max(sq_dists))
         return sim_thresholds
 
@@ -144,7 +144,31 @@ class Soinn(object):
         self.nodes = np.delete(self.nodes, indexes, 0)
         remained_indexes = list(set([i for i in range(n)]) - set(indexes))
         self.winning_times = [self.winning_times[i] for i in remained_indexes]
-        self.adjacent_mat = self.adjacent_mat[np.ix_(remained_indexes, remained_indexes)]
+        #old_ver_adjacent_mat = self.adjacent_mat[np.ix_(remained_indexes, remained_indexes)]
+        self.__update_adjacent_mat(indexes, n, len(remained_indexes))
+        #assert (old_ver_adjacent_mat.toarray() == self.adjacent_mat.toarray()).all()
+
+    def __update_adjacent_mat(self, indexes, prev_n, next_n):
+        while indexes:
+            next_adjacent_mat = dok_matrix((prev_n, prev_n))
+            for key1, key2 in self.adjacent_mat.keys():
+                if key1 == indexes[0] or key2 == indexes[0]:
+                    continue
+                if key1 > indexes[0]:
+                    new_key1 = key1 - 1
+                else:
+                    new_key1 = key1
+                if key2 > indexes[0]:
+                    new_key2 = key2 - 1
+                else:
+                    new_key2 = key2
+                #Because dok_matrix.__getitem__ is slow,
+                #access as dictionary.
+                next_adjacent_mat[new_key1, new_key2] = super(dok_matrix, self.adjacent_mat).__getitem__((key1, key2))
+            self.adjacent_mat = next_adjacent_mat.copy()
+            indexes = [i-1 for i in indexes]
+            indexes.pop(0)
+        self.adjacent_mat.resize((next_n, next_n))
 
     def __delete_noise_nodes(self):
         n = len(self.winning_times)
@@ -152,4 +176,5 @@ class Soinn(object):
         for i in range(n):
             if len(self.adjacent_mat[i, :]) < self.min_degree:
                 noise_indexes.append(i)
-        self.__delete_nodes(noise_indexes)
+        if noise_indexes:
+            self.__delete_nodes(noise_indexes)
